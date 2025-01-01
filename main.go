7ad4d65377385d21633870908e9e3f9054ac89d9
h1kye <shuttlebox@hotmail.com>
2025-01-01 19:50:36 +0000

    goroutine - BASIC CHANNELS - no waitgroups - good

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/gocolly/colly"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 )
 
+// -----------------------
 // WEBSCRAPER - GOROUTINES
 // -----------------------
 // GOROUTINE - EXTRACT MULTIPLE URLS, FROM LIST THEN RUN IN PARALLEL
@@ -16,52 +18,73 @@ import (
 
 func main() {
 
+	// URL LIST
+	urlList := []string{"wirefast.com", "itv.com", "sky.com", "channel4.com"}
+
+	// CHANNEL CREATE
+	resultsChan := make(chan string)
+
 	//TIME EXECUTION
 	startTime := time.Now()
 
 	// SCRAPER RUN
-	scraperUrl()
+	for _, siteName := range urlList {
+		// > JOIN "HTTPS://" TO SITE URL
+		fullURL := strings.Join([]string{"https://", siteName}, "")
 
+		// GOROUTINE SCRAPER RUN - SEND RESULTS TO "CHANNEL"
+		go scraperUrl(fullURL, resultsChan)
+	}
+
+	// CHANNEL RECEIVE - PRINT RESULTS
+	fmt.Printf(<-resultsChan)
+
+	// TOTAL DURATION RUN
 	durationRun := time.Since(startTime)
-	fmt.Printf("TOTAL RUNTIME - %v ns - %.2f seconds\n", durationRun, durationRun.Seconds())
+	fmt.Printf("\nTOTAL RUNTIME - %v ns - %.2f seconds\n", durationRun, durationRun.Seconds())
 }
 
-// MARK -> HOW TO CREAT GOROUTINES + CHANNELS???
-
-func scraperUrl() {
+// URL SCRAPE
+// -----------
+//
+//	 [ARGS]: STRING -> url
+//			 CHANNEL -> "send" data to channel (outbound)
+func scraperUrl(site string, chSexy chan<- string) {
 
 	// CREATE HANDLER TO PULL URL!!
-	urlList := []string{"wirefast.com", "itv.com", "sky.com", "channel4.com"}
 	c := colly.NewCollector()
 
-	// LIST OF SITES - SCRAPE
-	for _, site := range urlList {
-		fmt.Printf("This is the beginning URL ..... %v\n", site)
+	fmt.Printf("This is the beginning URL ..... %v\n", site)
 
-		// OnResponse callback - RETURN PAGE CONTENTS
-		c.OnResponse(func(r *colly.Response) {
+	// OnResponse callback - RETURN PAGE CONTENTS
+	c.OnResponse(func(r *colly.Response) {
 
-			// DISPLAY - FULL PAGE CONTENTS
-			pageFullContents := string(r.Body)
-			fmt.Printf("FULL PAGE CONTENTS - %v \n", pageFullContents)
+		// DISPLAY - FULL PAGE CONTENTS
+		pageFullContents := string(r.Body)
+		fmt.Printf("FULL PAGE CONTENTS - %v\n %v\n", pageFullContents)
 
-			// DISPLAY - "RETURN CODE"
-			statusCode := r.StatusCode
-			fmt.Printf("STATUS CODE - %v\n", statusCode)
-		})
+		// DISPLAY - "RETURN CODE"
+		statusCode := r.StatusCode
+		fmt.Printf("STATUS CODE - %v\n", statusCode)
 
-		// CONNECT TO SITE
-		//
-		// > JOIN "HTTPS://" TO SITE URL
-		fullURL := strings.Join([]string{"https://", site}, "")
-		// > CONNECT TO SITE URL
-		result := c.Visit(fullURL)
+		// SEND TO CHANNEL - RETURN CODE (CONVERT FROM INT TO STRING)
+		chSexy <- strconv.Itoa(statusCode)
+	})
 
-		// CHECK CONNECTION STATUS
-		if result != nil {
-			fmt.Printf("Error extracting page - %s", result)
-			//continue
-			log.Fatal(result)
-		}
+	// CONNECT TO SITE
+	//
+	// > CONNECT TO SITE URL
+	result := c.Visit(site)
+
+	// CHECK CONNECTION STATUS
+	if result != nil {
+		fmt.Printf("Error extracting page - %s", result)
+		//continue
+		log.Fatal(result)
 	}
 }
+
+// [MARK]
+// VERSION 1 - GROUTINE (NO WAITGROUPS!!) -> BASIC SETUP
+//
+// to do --> intorduce "waitgroups" --> more complex channel management
